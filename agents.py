e2c853ed7807d04009e7b190ab03541f09c9dd91
dsalian <dsalian@gmail.com>
2026-02-08 15:44:15 -0700

    Connecting all the tools

--- agents.py
+++ agents.py
@@ -1,8 +1,15 @@
 from langchain.agents import create_agent
-from tools import git_tools as git_tool
+from tools.tickets_tool import search_tickets
+from tools.git_tools import git_log
+from tools.code_tools import search_code 
 
+SYSTEM_PROMPT = f"""
+      You are a helpful agent - Your name is Bravo! Be friendly
+      You help engineers find information about tickets, git commits, and code.
+      """
 agent = create_agent(
     model="gpt-3.5-turbo",
-    system_prompt= "You are a helpful agent - Your name is Bravo! Be friendly",
-    tools =[git_tool]
+    system_prompt= SYSTEM_PROMPT,
+    tools =[git_log,search_code,search_tickets]
 )
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, ToolMessage
 load_dotenv() 
 
 
@@ -7,11 +8,20 @@ from agents import agent
 def main():
 
    response = agent.invoke(
-    {'messages':[{"role":"user", "content":"Get the last 20 commits from engineer id - 12345?"}]}
+    {'messages':[{"role":"user", "content":"What was the last commit by Manoj"}]}
    )
 
-   print(response)
+   print(response["messages"][-1].content)
 
+
+   print(f"Details are here....................")
+   for messages in response["messages"]:
+      if isinstance(messages,AIMessage):
+         print(f"AI messages - AI Message Content {messages.content} \n argument we are searching is {messages.tool_calls} ")
+      if isinstance(messages, ToolMessage):
+         print(f"AI messages - tool names  {messages.name} \n )")
+         print(f"AI messages - tool calls are {messages.name}")
 
 if __name__ == "__main__":
     main()
+
