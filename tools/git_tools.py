00548f735ec3ae5c7cc4830955bd1f6f1c9fc6b8
dsalian <dsalian@gmail.com>
2026-02-08 15:43:40 -0700

    Git tool for analysis

---
+++ tools/git_tools.py
@@ -0,0 +1,25 @@
+import subprocess
+from sys import stderr
+from langchain.tools import tool
+
+@tool
+def git_log(author: str=None, num_of_commits: int=10):
+    """
+        Get the list of git commits by the developer
+        Args:
+            author: An authors name for filters
+                    Example: 'MK', 'dsalian'
+            
+            num_of_commits: An integer to limit the fetch the commits 
+                    Example: 5,10
+    """
+    try:
+        output = subprocess.run(["git", "log", "--oneline", "-n", str(num_of_commits), "--author", author], capture_output=True, text=True)
+
+        if output.stdout:
+            return output.stdout
+        elif output.stderr:
+            raise Exception("Git logging exception")
+    except Exception as e:
+        print(f"Exception while reading git document {e}")
+   
