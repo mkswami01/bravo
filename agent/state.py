c9a8f282c74282bf2342ca0e448ae1f46993629e
Bond <jamesbond@gmail.com>
2026-02-09 12:18:03 -0700

    added state for graph and structured response

---
+++ agent/state.py
@@ -0,0 +1,11 @@
+from typing import Literal, TypedDict
+
+class AgentState(TypedDict):
+    """The input and output state for the subagents"""
+    query: str
+    result: str
+    source: str
+
+class ClassificatonState(TypedDict):
+    """Routing classification on what agent to be called"""
+    source: Literal["git", "code", "tickets"]
