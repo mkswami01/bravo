b140c8075aea96142c0c2d842f42da0425b1976c
Manoj Kumar <mkswami01@gmail.com>
2026-02-17 06:42:13 -0700

    getting briefs - seq calling git and linear calls

---
+++ agent/briefs.py
@@ -0,0 +1,19 @@
+from langchain.agents import create_agent
+from agent.state import AgentState
+from tools.briefs import daily_brief
+
+
+def briefs(state:AgentState) -> dict:
+
+    agent = create_agent(
+        model="gpt-3.5-turbo", 
+        system_prompt="""
+            You are an assitant to startup CTO.
+            Your job is to send a daily brief based on the git commits, pull requests and the issues that were addressed.
+            """,
+        tools=[daily_brief]
+    )
+
+    response = agent.invoke({"messages": [{"role":"user", "content":state["query"]}]})
+    
+    return {"result":response["messages"][-1].content}
