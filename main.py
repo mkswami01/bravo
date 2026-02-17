4a7397cf0b2499bea5e8197e3b5c500dfa0eab66
Manoj Kumar <mkswami01@gmail.com>
2026-02-17 06:56:09 -0700

    adding briefs node

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
-from langchain_core.messages import AIMessage, ToolMessage
 from langgraph.constants import END, START
 from langgraph.graph.state import StateGraph
 load_dotenv() 
 
+from agent.briefs import briefs
 from agent.linear_agent import linear_agent
 from agent.git_agent import git_agent
 from agent.router import classify_query, routing_condition
@@ -13,6 +13,7 @@ builder = StateGraph(AgentState)
 builder.add_node("classifier", classify_query)
 builder.add_node("git", git_agent)
 builder.add_node("linear", linear_agent)
+builder.add_node("briefs", briefs)
 
 builder.add_edge(START, "classifier")
 builder.add_conditional_edges("classifier",routing_condition)
@@ -22,7 +23,7 @@ builder.add_edge("linear", END)
 graph = builder.compile()
 
 def main():
-   state = AgentState(query="What work is completed so far ?", result="", source="")
+   state = AgentState(query="Give me a daily briefs please", result="", source="")
 
    response = graph.invoke(state)
 
