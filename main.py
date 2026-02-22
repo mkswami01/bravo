eaddf2e8ebbe004f65acda49c5261b76c5f24c9a
Manoj Kumar <mkswami01@gmail.com>
2026-02-21 20:40:39 -0700

    classification is handled here

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from dotenv import load_dotenv
 load_dotenv() 
-   
+
 from graph.plan_graph import PlanAndExecute
-from agent.planner import plan_and_execute
-from agent.briefs import briefs
-from agent.linear_agent import linear_agent
-from agent.git_agent import git_agent
-from agent.router import SYSTEM_PROMPT, classify_query, routing_condition
-from agent.state import AgentState, PlannerState
+from graph.graph_initializer import Initializer
+from agent.state import ClassificationState, PlannerState
+from graph.react_graph import Agent
 
 
 def main():
   
-  state = PlannerState(query="what work was completed yesterday? How many tickets were done ?", plan=[], results=[], result="")
+  state = ClassificationState(query="What was was recently completed by mkumar ? Does he have any tickets assigned ? In linear he might be referred to Manoj Kumar", domain="", complexity="")
 
-  agent = PlanAndExecute()
-  agent.plan_and_execute(state)
+  agent = Initializer()
+  state = agent.run(state)
 
-  print(f"state \n {state}")
+  if state["complexity"] == "complex":
 
+    planner = PlannerState(query=state["query"], plan=[], results=[], result=[])
+    pne = PlanAndExecute()
+    final_state = pne.run(planner)
+    print(f"User query : {final_state['query']}")
+    print(f"{final_state["results"]}")
+
+  elif state ["complexity"] == "simple":
+
+    react_state = {"query": state["query"], "result": "", "domain": ""}
+    react_agent = Agent()
+    final_state = react_agent.run(react_state)
+
+    print(f"User query : {final_state['query']}")
+    print(f"{final_state["result"]}")
 
 if __name__ == "__main__":
     main()
