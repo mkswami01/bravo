e878797f75254ecfab8f96175e2a05fe055aeba7
Manoj Kumar <mkswami01@gmail.com>
2026-02-22 15:11:29 -0700

    updated the model for demo

--- agent/git_agent.py
+++ agent/git_agent.py
@@ -6,7 +6,7 @@ from tools.github_tools import git_commits, git_pull_requests
 def git_agent(state:AgentState) -> dict:
 
     agent = create_agent(
-        model="gpt-3.5-turbo", 
+        model="gpt-4o", 
         system_prompt=f"""
             You are a git specialist with access to GitHub tools to fetch commits and PRs.
             You can filter by authors. Take a look at the work done, summarize it, and keep details.
--- agent/linear_agent.py
+++ agent/linear_agent.py
@@ -8,7 +8,7 @@ from langchain.agents import create_agent
 def linear_agent(state:AgentState) -> dict:
 
     agent = create_agent(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         system_prompt= f"""
         you are an expert in Linear project management tool. You can fetch what issues are assiged by assignee name and state.
         Take a look the issues, summarize it. Give the details 
